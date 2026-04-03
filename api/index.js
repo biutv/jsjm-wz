940d911bfcb236f243555ab8b9a9e60525669a69
biutv <108448101+biutv@users.noreply.github.com>
2026-04-03 22:06:00 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-app.use(express.json()); // 替代 body-parser
-
-// 健康检查
-app.get('/', (req, res) => {
-    res.json({ status: 'ok', service: 'js-decoder' });
-});
-
-module.exports.handler = serverless(app);
+module.exports = (req, res) => {
+  res.status(200).send('Hello World');
+};
