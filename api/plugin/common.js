b528837e92bc84776ba6ec7ecd5a11db27e0425f
biutv <108448101+biutv@users.noreply.github.com>
2026-04-03 15:38:59 +0800

    Update common.js

--- api/plugin/common.js
+++ api/plugin/common.js
@@ -1,25 +1,27 @@
-import { parse } from '@babel/parser'
-import _generate from '@babel/generator'
-const generator = _generate.default
-import _traverse from '@babel/traverse'
-const traverse = _traverse.default
-import deleteUnreachableCode from '../visitor/delete-unreachable-code.js'
-import deleteNestedBlocks from '../visitor/delete-nested-blocks.js'
-import calculateConstantExp from '../visitor/calculate-constant-exp.js'
-import calculateRString from '../visitor/calculate-rstring.js'
+const { parse } = require('@babel/parser');
+const _generate = require('@babel/generator');
+const generator = _generate.default;
+const _traverse = require('@babel/traverse');
+const traverse = _traverse.default;
+const deleteUnreachableCode = require('../visitor/delete-unreachable-code.js');
+const deleteNestedBlocks = require('../visitor/delete-nested-blocks.js');
+const calculateConstantExp = require('../visitor/calculate-constant-exp.js');
+const calculateRString = require('../visitor/calculate-rstring.js');
 
-export default function (code) {
-  let ast
+function decodeCommon(code) {
+  let ast;
   try {
-    ast = parse(code, { errorRecovery: true })
+    ast = parse(code, { errorRecovery: true });
   } catch (e) {
-    console.error(`Cannot parse code: ${e.reasonCode}`)
-    return null
+    console.error(`Cannot parse code: ${e.reasonCode}`);
+    return null;
   }
-  traverse(ast, deleteUnreachableCode)
-  traverse(ast, deleteNestedBlocks)
-  traverse(ast, calculateConstantExp)
-  traverse(ast, calculateRString)
-  code = generator(ast).code
-  return code
+  traverse(ast, deleteUnreachableCode);
+  traverse(ast, deleteNestedBlocks);
+  traverse(ast, calculateConstantExp);
+  traverse(ast, calculateRString);
+  code = generator(ast).code;
+  return code;
 }
+
+module.exports = decodeCommon;
