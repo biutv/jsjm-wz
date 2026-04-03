0a70d80b77e1df0aad32011d2ee21e7477df7770
biutv <108448101+biutv@users.noreply.github.com>
2026-04-03 21:55:09 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-module.exports = (req, res) => {
-  res.status(200).json({ status: 'ok' });
-};
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+app.get('/', (req, res) => res.json({ status: 'ok' }));
+
+module.exports.handler = serverless(app);
