6ce0143d6c2cd0c7f5d276633e95ec957aeed25c
biutv <108448101+biutv@users.noreply.github.com>
2026-04-03 21:18:05 +0800

    Update split-assignment.js

--- api/visitor/split-assignment.js
+++ api/visitor/split-assignment.js
@@ -1,66 +1,66 @@
-import * as t from '@babel/types'
+const t = require('@babel/types');
 
 function getInsertPath(path) {
-  let insertPath = path
-  let parent = insertPath.parentPath
-  let needSplit = false
+  let insertPath = path;
+  let parent = insertPath.parentPath;
+  let needSplit = false;
   while (parent && !parent.isBlockStatement() && !parent.isProgram()) {
-    let valid = false
+    let valid = false;
     if (parent.isAssignmentExpression()) {
-      valid = true
-      needSplit = true
+      valid = true;
+      needSplit = true;
     }
     if (parent.isCallExpression()) {
       if (insertPath.key === 'callee') {
-        valid = true
-        needSplit = true
+        valid = true;
+        needSplit = true;
       }
     }
     if (parent.isExpressionStatement()) {
-      valid = true
+      valid = true;
     }
     if (parent.isIfStatement()) {
       if (insertPath.key === 'test') {
-        valid = true
-        needSplit = true
+        valid = true;
+        needSplit = true;
       }
     }
     if (parent.isMemberExpression()) {
-      valid = true
-      needSplit = true
+      valid = true;
+      needSplit = true;
     }
     if (parent.isVariableDeclarator()) {
       if (insertPath.key === 'init') {
-        valid = true
-        needSplit = true
+        valid = true;
+        needSplit = true;
       }
     }
     if (parent.isVariableDeclaration()) {
       if (insertPath.key === 0) {
-        valid = true
-        needSplit = true
+        valid = true;
+        needSplit = true;
       }
     }
     if (!valid) {
-      return undefined
+      return undefined;
     }
-    insertPath = parent
-    parent = insertPath.parentPath
+    insertPath = parent;
+    parent = insertPath.parentPath;
   }
   if (!needSplit) {
-    return undefined
+    return undefined;
   }
-  return insertPath
+  return insertPath;
 }
 
 function procAssignment(path) {
-  const insertPath = getInsertPath(path)
+  const insertPath = getInsertPath(path);
   if (!insertPath) {
-    return
+    return;
   }
-  insertPath.insertBefore(t.expressionStatement(path.node))
-  path.replaceWith(path.node.left)
-  insertPath.scope.crawl()
+  insertPath.insertBefore(t.expressionStatement(path.node));
+  path.replaceWith(path.node.left);
+  insertPath.scope.crawl();
 }
 
 /**
@@ -69,6 +69,6 @@ function procAssignment(path) {
  * - In the test of IfStatement
  * - In the VariableDeclaration
  */
-export default {
+module.exports = {
   AssignmentExpression: procAssignment,
-}
+};
