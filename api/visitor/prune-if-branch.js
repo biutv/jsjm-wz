c35afa4b30aed244cca10429605544c91f573ac5
biutv <108448101+biutv@users.noreply.github.com>
2026-04-03 21:16:41 +0800

    Update prune-if-branch.js

--- api/visitor/prune-if-branch.js
+++ api/visitor/prune-if-branch.js
@@ -2,30 +2,30 @@ function pruneIfBranch(path) {
   function clear(path, toggle) {
     // 判定成立
     if (toggle) {
-      path.replaceWith(path.node.consequent)
-      return
+      path.replaceWith(path.node.consequent);
+      return;
     }
     // 判定不成立
     if (!path.node.alternate) {
-      path.remove()
-      return
+      path.remove();
+      return;
     }
-    path.replaceWith(path.node.alternate)
+    path.replaceWith(path.node.alternate);
   }
   // 判断判定是否恒定
-  const test = path.node.test
-  const types = ['StringLiteral', 'NumericLiteral', 'BooleanLiteral']
+  const test = path.node.test;
+  const types = ['StringLiteral', 'NumericLiteral', 'BooleanLiteral'];
   if (test.type === 'BinaryExpression') {
     if (
       types.indexOf(test.left.type) !== -1 &&
       types.indexOf(test.right.type) !== -1
     ) {
-      const left = JSON.stringify(test.left.value)
-      const right = JSON.stringify(test.right.value)
-      clear(path, eval(left + test.operator + right))
+      const left = JSON.stringify(test.left.value);
+      const right = JSON.stringify(test.right.value);
+      clear(path, eval(left + test.operator + right));
     }
   } else if (types.indexOf(test.type) !== -1) {
-    clear(path, eval(JSON.stringify(test.value)))
+    clear(path, eval(JSON.stringify(test.value)));
   }
 }
 
@@ -34,7 +34,7 @@ function pruneIfBranch(path) {
  *
  * The code must be reloaded to update the references
  */
-export default {
+module.exports = {
   IfStatement: pruneIfBranch,
   ConditionalExpression: pruneIfBranch,
-}
+};
