27446989b86fb53042b464f025ad011d1076dff0
biutv <108448101+biutv@users.noreply.github.com>
2026-04-03 21:10:11 +0800

    Update calculate-rstring.js

--- api/visitor/calculate-rstring.js
+++ api/visitor/calculate-rstring.js
@@ -1,37 +1,39 @@
-import _generate from '@babel/generator'
-const generator = _generate.default
-import * as t from '@babel/types'
+const _generate = require('@babel/generator');
+const generator = _generate.default;
+const t = require('@babel/types');
 
 /**
  * "sh".split("").reverse().join("") -> "hs"
  */
-export default {
-  StringLiteral(path) {
-    if (path.key !== 'object') {
-      return
+function calculateRString(path) {
+  if (path.key !== 'object') {
+    return;
+  }
+  let root = path;
+  let count = 6;
+  while (root.parentPath && count) {
+    if (
+      root.parentPath.isMemberExpression() ||
+      root.parentPath.isCallExpression()
+    ) {
+      root = root.parentPath;
+      --count;
+    } else {
+      break;
     }
-    let root = path
-    let count = 6
-    while (root.parentPath && count) {
-      if (
-        root.parentPath.isMemberExpression() ||
-        root.parentPath.isCallExpression()
-      ) {
-        root = root.parentPath
-        --count
-      } else {
-        break
-      }
-    }
-    if (count) {
-      return
-    }
-    const code = generator(root.node).code
-    try {
-      const ret = eval(code)
-      root.replaceWith(t.stringLiteral(ret))
-    } catch {
-      //
-    }
-  },
+  }
+  if (count) {
+    return;
+  }
+  const code = generator(root.node).code;
+  try {
+    const ret = eval(code);
+    root.replaceWith(t.stringLiteral(ret));
+  } catch {
+    //
+  }
 }
+
+module.exports = {
+  StringLiteral: calculateRString,
+};
